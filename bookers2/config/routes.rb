ba725ec7504267392198e6f5728af1b5faa7f8b5
yoshikawakoki <cooler970708@gmail.com>
2021-10-25 08:32:01 +0000

    ヘッダー・フッター作成　Home Url違う

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:index, :create, :show, :edit, :destroy, :update]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index,:show, :edit, :update]
+  get "/home/about" => "homes#about"
 end
