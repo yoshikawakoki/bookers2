8aaf3380abaa040fb249d3f7e57a05b4376eb229
yoshikawakoki <cooler970708@gmail.com>
2021-10-27 13:21:21 +0000

    create機能作成

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
 
   def create
-
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
@@ -18,6 +22,12 @@ class BooksController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
