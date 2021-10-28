0dda2fa5e83c3f16f3c0af2d7181d57a75c496f8
yoshikawakoki <cooler970708@gmail.com>
2021-10-28 05:05:23 +0000

    book edit作成

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -13,9 +13,12 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+
+  end
+
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
